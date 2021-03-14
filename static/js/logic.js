486c86fc6e0f88441f3ee9c26bd71437d69d6931
David Andrew Carr <52014240+davidandcarr@users.noreply.github.com>
2021-03-14 12:32:21 -0500

    finish

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,12 +2,7 @@
 console.log("working");
 
 //create map object
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
+let map = L.map("mapid").setView([34.0522, -118.2437], 13);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -15,6 +10,15 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     maxZoom: 18,
     accessToken: API_KEY
 });
+
+let marker = L.circle([34.0522, -118.2437], {
+  radius: 200,
+  color: "black",
+  fillColor: '#ffffa1'
+}).addTo(map)
+
+plateLink = "https://github.com/fraxen/tectonicplates/blob/master/original/PB2002_boundaries.dig.txt";
+
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
